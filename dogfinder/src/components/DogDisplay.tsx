77696eab07d12ba70e5c18823da3fd35ec3134e9
Necolanch <necolanch@gmail.com>
2023-08-08 16:47:38 -0400

    Fix DogDisplay component to show data on initial load

--- dogfinder/src/components/DogDisplay.tsx
+++ dogfinder/src/components/DogDisplay.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { APIGateway } from "../services/APIGateway";
 import { DogService } from "../services/dog/dogService";
 import { Dog } from "../services/dog/IDog";
-import { useAppDispatch } from "../hooks";
+import { useAppDispatch, useAppSelector } from "../hooks";
 import { setIds } from "../features/dog/dogSlice";
 
 const DogDisplay = () => {
@@ -11,6 +11,8 @@ const DogDisplay = () => {
     const dispatch = useAppDispatch();
     const [dogs, setDogs] = useState<Dog[]>([]);
     const [loading, setLoading] = useState(false);
+    const resultIds = useAppSelector(state => state.dog.resultIds);
+    console.log(dogs);
 
     useEffect(() => {
         dogService.DogDefault()
@@ -18,17 +20,21 @@ const DogDisplay = () => {
                 dispatch(setIds(response.resultIds))
             })
 
+
+    }, [])
+
+    useEffect(() => {
         dogService.GetDogs()
             .then(res => res.json())
             .then(data => setDogs(data))
 
         setLoading(true)
-    }, [])
+    }, [resultIds]);
 
     return (
         <>
             {
-                loading ? (dogs.length > 0) && dogs.map(dog => {
+                loading ? dogs.map(dog => {
                     return (
                         <section key={dog.id}>
                             <img src={dog.img} alt="Cute doggy" />
